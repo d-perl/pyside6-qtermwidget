a3c4f0fc5f83ef971650343d482aa81ee2666b96
wakonig_k <klaus.wakonig@psi.ch>
2026-02-08 11:32:30 +0100

    wip - cmake cleanup multiple files

---
+++ cmake/QTermWidget.cmake
@@ -0,0 +1,30 @@
+# QTermWidget Configuration
+# This file handles building qtermwidget as an external project
+
+# Set paths for qtermwidget
+set(EXT_QTERMWIDGET_DIR "${CMAKE_CURRENT_SOURCE_DIR}/external/qtermwidget")
+set(QTERMWIDGET_SOURCE_DIR "${CMAKE_CURRENT_SOURCE_DIR}/../qtermwidget")
+set(QTERMWIDGET_BUILD_DIR "${CMAKE_CURRENT_BINARY_DIR}/qtermwidget-build")
+set(QTERMWIDGET_DIR "${QTERMWIDGET_BUILD_DIR}/install")
+
+# Determine library extension based on platform
+if(APPLE)
+    set(QTERMWIDGET_LIB "${QTERMWIDGET_DIR}/lib/libqtermwidget6.dylib")
+else()
+    set(QTERMWIDGET_LIB "${QTERMWIDGET_DIR}/lib/libqtermwidget6.so")
+endif()
+
+# Build qtermwidget as an external project
+ExternalProject_Add(qtermwidget_external
+    DEPENDS lxqt_build_tools_external
+    SOURCE_DIR ${QTERMWIDGET_SOURCE_DIR}
+    BINARY_DIR ${QTERMWIDGET_BUILD_DIR}
+    CMAKE_ARGS
+        -DCMAKE_PREFIX_PATH=${QT6_INSTALL_DIR}
+        -DCMAKE_INSTALL_PREFIX=${QTERMWIDGET_DIR}
+        -DCMAKE_INSTALL_RPATH=$ORIGIN
+        -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
+    BUILD_COMMAND ${CMAKE_COMMAND} --build ${QTERMWIDGET_BUILD_DIR}
+    INSTALL_COMMAND ${CMAKE_COMMAND} --install ${QTERMWIDGET_BUILD_DIR}
+    BUILD_BYPRODUCTS ${QTERMWIDGET_LIB}
+)
