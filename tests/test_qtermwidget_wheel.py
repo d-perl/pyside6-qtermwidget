9964493870be829778e1c955506ccc07d278d7d2
perl_d <david.perl@psi.ch>
2026-03-24 09:40:35 +0100

    wip

--- tests/test_qtermwidget_wheel.py
+++ tests/test_qtermwidget_wheel.py
@@ -6,13 +6,3 @@ def test_module_has_class():
     import pyside6_qtermwidget
 
     assert hasattr(pyside6_qtermwidget, "QTermWidget")
-
-
-def test_instantiate_class():
-    import sys
-
-    import pyside6_qtermwidget
-    from PySide6.QtWidgets import QApplication
-
-    app = QApplication(sys.argv)
-    widget = QTermWidget()
